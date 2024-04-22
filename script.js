f8000f4991e9cb75da6f14e0cf5bbeed09864a45
Gustavo <gustavo_floriano1@estudante.sesisenai.org.br>
2024-04-22 08:57:56 -0300

    coloquei mais alguns comentarios no javascript

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ var botao_calculo = document.getElementById('botao');
 var calculo = 0;
 
 botao_calculo.addEventListener('click', function(){ // addEventListener faz com que adicione um evento que nesse caso, entre parenteses é o "click" e ao clicar, realiza a function.
-    var numero1 = Number(input1.value); // value faz com que tenha acesso somente ao que o usuário digitou
+    var numero1 = Number(input1.value); // value faz com que tenha acesso somente ao que o usuário escolheu ou digitou
     var numero2 = Number(input2.value);
     var operacao_selecionada = operacao.value;
     switch (operacao_selecionada){ // swicth e case é referente a escolha caso do portugol
